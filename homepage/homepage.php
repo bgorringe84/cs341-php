64c46b7ca5b1fca370003e926060f4742fa7ddfc
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-11 20:01:45 -0700

    new project

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -13,7 +13,7 @@
 <?php
 $d1=strtotime("July 04");
 $d2=ceil(($d1-time())/60/60/24);
-echo "There are " . $d2 ." days until 4th of July.";
+echo "There are " . $d2 ." days until 4th of July!";
 ?>
 </body>
 </html>
