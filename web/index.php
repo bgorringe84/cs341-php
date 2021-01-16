9498e11693af92e6d488ea337c1acaea598aefc9
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-15 22:43:55 -0700

    changed size of image in card

--- web/index.php
+++ web/index.php
@@ -44,7 +44,7 @@
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="profile.jpg" alt="Brandon Gorringe">
+        <img src="profile.jpg" class="img-fluid" alt="Brandon Gorringe">
       </div>
       <div class="col-md-8">
         <div class="card-body">
