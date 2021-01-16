017320909d2f265e286cc8226be20bb944e54a21
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-15 22:42:08 -0700

    added pic to card

--- web/index.php
+++ web/index.php
@@ -44,13 +44,12 @@
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="..." alt="...">
+        <img src="profile.jpg" alt="Brandon Gorringe">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">Brandon Gorringe</h5>
           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
       </div>
     </div>
