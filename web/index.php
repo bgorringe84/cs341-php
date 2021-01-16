a5308a07867553dbc28b628088f98c2c1d9feaaf
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-15 21:29:21 -0700

    added navbar text

--- web/index.php
+++ web/index.php
@@ -13,7 +13,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <a class="navbar-brand" href="#">CSE 341 Web Backend Development II</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -23,14 +23,14 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Assignments</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Links</a>
         </li>
       </ul>
       <span class="navbar-text">
-        Navbar text with an inline element
+        Brandon Gorringe
       </span>
     </div>
   </div>
