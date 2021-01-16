af41dc43dd4592b6470668e0c6f29f62c75847c0
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-15 22:48:44 -0700

    card image rescaled

--- web/index.php
+++ web/index.php
@@ -41,12 +41,12 @@
 </nav>
 
 <div class="container">
-  <div class="card mb-3" style="max-width: 540px;">
+  <div class="card lg-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-lg-4">
         <img src="profile.jpg" class="img-fluid" alt="Brandon Gorringe">
       </div>
-      <div class="col-md-8">
+      <div class="col-lg-8">
         <div class="card-body">
           <h5 class="card-title">Brandon Gorringe</h5>
           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
