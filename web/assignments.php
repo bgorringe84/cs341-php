c99e0830d0b3006a267aeae177c3ed36cfeab8cd
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-15 21:57:45 -0700

    added assignments page link

--- web/assignments.php
+++ web/assignments.php
@@ -20,10 +20,10 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="assignments.php>Assignments</a>
+          <a class="nav-link active" href="assignments.php">Assignments</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Links</a>
