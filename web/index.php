a6f4e4b0391fcd7b4aaf479ebfd556ba73b6ee22
Brandon Gorringe <bgorringe84@gmail.com>
2021-01-16 18:23:34 -0700

    new attempt at profile pic

--- web/index.php
+++ web/index.php
@@ -40,7 +40,39 @@
   </div>
 </nav>
 
-<div class="container-fluid">
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-2">
+      1 of 3
+    </div>
+    <div class="col-md-auto">
+      Variable width content
+    </div>
+    <div class="col col-lg-2">
+      3 of 3
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-md-auto">
+      <div class="card bg-dark text-white">
+        <img src="profile.jpg" class="img-responsive" alt="Brandon Gorringe">
+        <div class="card-img-overlay">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text">Last updated 3 mins ago</p>
+        </div>
+      </div>
+    </div>
+    <div class="col col-lg-2">
+      3 of 3
+    </div>
+  </div>
+</div>
+
+<!-- <div class="container-fluid">
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-lg-4">
@@ -54,7 +86,7 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 
 <script src="jquery-3.5.1.min.js"></script>
